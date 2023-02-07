02690e7d8c668b9d0541f35af73ef8c3256dcc5b
ShinyaIshitobi <shinya.ishitobi@akariinc.co.jp>
2023-02-07 22:28:55 +0900

    refac: task definition

--- infra/terraform/ecs.tf
+++ infra/terraform/ecs.tf
@@ -8,34 +8,34 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
   task_role_arn            = aws_iam_role.ecs_task.arn
   container_definitions    = <<CONTAINERS
 [
-    {
-        "name": "${local.app}",
-        "image": "medpeer/health_checks:latest",
-        "portMapping": [
-            {
-                "hostPort": 8080,
-                "containerPort": 8080,
-            }
-        ],
-        "logConfiguration": {
-            "logDriver": "awslogs",
-            "options": {
-                "awslogs-group": "${aws_cloudwatch_log_group.cloudwatch_log_group.name}",
-                "awslogs-region": "${local.region}",
-                "awslogs-stream-prefix": "${local.app}"
-            }
-        },
-        "environment": [
-            {
-                "name": "NGINX_PORT",
-                "value": "8080"
-            },
-            {
-                "name": "HEALTH_CHECK_PATH",
-                "value": "/health_checks"
-            }
-        ]
-    }
+  {
+    "name": "${local.app}",
+    "image": "medpeer/health_check:latest",
+    "portMappings": [
+      {
+        "hostPort": 8080,
+        "containerPort": 8080
+      }
+    ],
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-group": "${aws_cloudwatch_log_group.cloudwatch_log_group.name}",
+        "awslogs-region": "${local.region}",
+        "awslogs-stream-prefix": "app"
+      }
+    },
+    "environment": [
+      {
+        "name": "NGINX_PORT",
+        "value": "8080"
+      },
+      {
+        "name": "HEALTH_CHECK_PATH",
+        "value": "/health_checks"
+      }
+    ]
+  }
 ]
 CONTAINERS
 }
